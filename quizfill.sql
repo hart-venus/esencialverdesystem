95529828a3a8212a9ed3edf79608540c48034bbf
Ariel Leyva <96149931+cherry-cakes@users.noreply.github.com>
2023-04-26 14:39:51 -0600

    region areas fill

--- quizfill.sql
+++ quizfill.sql
@@ -39,5 +39,11 @@ VALUES (1, 1), (2, 1), (3, 1), (4, 1), (5, 1),
        (1, 3), (2, 3), (3, 3), (4, 3), (5, 3),
        (1, 4), (2, 4), (3, 4), (4, 4), (5, 4);
 
+-- Insert data into the "region_areas" table
+INSERT INTO region_areas (region_id, country_id)
+VALUES (1, 1), (1, 2), (1, 3),
+       (2, 3), (2, 4),
+       (3, 5), (3, 6),
+       (4, 7), (4, 8), (4, 9);
 
 END;
