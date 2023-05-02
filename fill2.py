9d864e8fb11d7ba851e932d3ec0ed2753a249d80
Ariel Leyva <96149931+cherry-cakes@users.noreply.github.com>
2023-05-02 10:07:24 -0600

    fck

--- fill2.py
+++ fill2.py
@@ -6,4 +6,20 @@ database = 'esencialverdesystem'
 username = 'sa'
 password = 'Sven1234'
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+fake = Faker()
+cursor = cnxn.cursor()
+# producers: name
+producers = [(fake.company(),) for i in range(100)]
+# 1. delete all data from table
+cursor.execute("DELETE FROM producers")
+print(producers)
+# 2. insert new data
+cursor.executemany("INSERT INTO producers VALUES (NULL, ?, NULL, NULL, NULL)", producers)
+cnxn.commit()
+
+# products: name, kgs_to_produce
+products = [ (fake.company(), fake.random_int(1, 1000)) for i in range(1000)]
+cursor.execute("DELETE FROM products")
+cursor.executemany("INSERT INTO products VALUES (?, ?)", products)
+cnxn.commit()
 
