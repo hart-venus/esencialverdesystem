9fce65a10115ccfab885e6727d1d48cd68258e43
Ariel Leyva <96149931+cherry-cakes@users.noreply.github.com>
2023-05-02 08:29:09 -0600

    megaquery megamamada

--- megaquery.sql
+++ megaquery.sql
@@ -1,14 +1,14 @@
 SELECT
 producers.name as producer,
 products.name as product,
-SUM(product_price_per_location_log.price * currencies_dollar_exchange_rate_log.exchange_rate) AS total_dollars
+SUM(product_price_log.price * currencies_dollar_exchange_rate_log.exchange_rate) AS total_dollars
 
 FROM sales
 JOIN products ON sales.product_id = products.product_id
 JOIN recycling_contracts ON sales.recycling_contract_id = recycling_contracts.recycling_contract_id
 JOIN service_contracts ON recycling_contracts.service_contract_id = service_contracts.service_contract_id
 JOIN producers ON service_contracts.producer_id = producers.producer_id
-JOIN product_price_per_location_log ON product_price_per_location_log.product_id = products.product_id and product_price_per_location_log.location_id = sales.location_id and product_price_per_location_log.active = 1
-JOIN currencies_dollar_exchange_rate_log ON currencies_dollar_exchange_rate_log.currency_id = sales.currency_id and sales.datetime between currencies_dollar_exchange_rate_log.start_date and currencies_dollar_exchange_rate_log.end_date
+JOIN product_price_log ON product_price_log.product_id = products.product_id and product_price_log.active = 1
+JOIN currencies_dollar_exchange_rate_log ON currencies_dollar_exchange_rate_log.currency_id = product_price_log.currency_id and sales.datetime between currencies_dollar_exchange_rate_log.start_date and currencies_dollar_exchange_rate_log.end_date
 
 GROUP BY producers.name, products.name
