5786b3ca3107875897813cf627fd07f6c946a4e5
Ariel Leyva <aleyva1509@gmail.com>
2023-05-13 04:56:45 -0600

    get form params in TVP format

--- ui/app.py
+++ ui/app.py
@@ -1,42 +1,11 @@
 from flask import Flask, render_template, request
 import pyodbc
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
-    # Si se envía el formulario
-    if request.method == 'POST':
-        form_data = request.form
-        # save it to a file
-        with open('form_data.txt', 'w') as f:
-            f.write(str(form_data))
-
-        info_transportista = (form_data['transportista'], form_data['camion'], form_data['lugar'])
-
-        # vincular [0] con [0], [1] con [1], etc.
-        recolectas = zip(
-            form_data.getlist('tipo_residuo[]'),
-            form_data.getlist('tipo_recipiente_recolectar[]'),
-            form_data.getlist('cantidad_residuos[]')
-        )
-        # filter when cantidad_residuos == "" remove it from the list
-        recolectas = filter(lambda x: x[2] != "", recolectas)
-
-        entregas = zip(
-            form_data.getlist('tipo_recipiente_entregar[]'),
-            form_data.getlist('cantidad_recipientes[]')
-        )
-        # filter when cantidad_recipientes == "" remove it from the list
-        entregas = filter(lambda x: x[1] != "", entregas)
-
-        # save it to a file
-        with open('recolectas.txt', 'w') as f:
-            f.write(str(list(recolectas)))
-            f.write(str(list(entregas)))
-            f.write(str(info_transportista))
-
 
     # Establecer la cadena de conexión
     server = 'localhost'
@@ -48,6 +17,79 @@ def index():
     # Conectar a la base de datos
     conn = pyodbc.connect(conn_str)
     cursor = conn.cursor()
+
+    """
+    TVP REFERENCE
+        CREATE TYPE CollectorInfo AS TABLE
+        (
+            Nombre VARCHAR(255),
+            Placa VARCHAR(255),
+            Lugar VARCHAR(255)
+        );
+
+        CREATE TYPE RecipientLogInfo AS TABLE
+        (
+            TipoRecipiente VARCHAR(255),
+            Peso DECIMAL(12, 4),
+            TipoResiduo VARCHAR(255),
+            Accion INT,
+            RecipienteID INT NULL,
+            TipoRecipienteID INT NULL,
+            TipoResiduoID INT NULL
+        );
+    """
+    # Si se envía el formulario
+    if request.method == 'POST':
+        form_data = request.form
+
+        TransportistaTVP = [(form_data['transportista'], form_data['camion'], form_data['lugar'])]
+
+        # vincular [0] con [0], [1] con [1], etc.
+        recolectas = zip(
+            form_data.getlist('tipo_residuo[]'),
+            form_data.getlist('tipo_recipiente_recolectar[]'),
+            form_data.getlist('cantidad_residuos[]')
+        )
+        # filter when cantidad_residuos == "" remove it from the list
+        recolectas = filter(lambda x: x[2] != "", recolectas)
+
+        RecolectasData = [
+            (
+                r[1],
+                r[2],
+                r[0],
+                1, # Accion de recolectar
+                None,
+                None,
+                None
+            ) for r in recolectas
+        ]
+
+        entregas = zip(
+            form_data.getlist('tipo_recipiente_entregar[]'),
+            form_data.getlist('cantidad_recipientes[]')
+        )
+        # filter when cantidad_recipientes == "" remove it from the list
+        entregas = filter(lambda x: x[1] != "", entregas)
+
+        EntregasData = [
+            (
+                r[0],
+                r[1],
+                None,
+                2, # Accion de entregar
+                None,
+                None,
+                None
+            ) for r in entregas
+        ]
+
+        # create the TVP
+        RecipientTVP = RecolectasData + EntregasData
+
+
+
+
 
     # Ejecutar las consultas
     cursor.execute('SELECT full_name FROM people')
