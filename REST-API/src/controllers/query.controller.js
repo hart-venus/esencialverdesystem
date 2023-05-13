ea43860ab182d243325ca7d1043968515174fa40
TomasG10 <94805682+TomasG10@users.noreply.github.com>
2023-05-13 00:33:28 -0600

    final commit before mirroring

--- REST-API/src/controllers/query.controller.js
+++ REST-API/src/controllers/query.controller.js
@@ -5,9 +5,6 @@ export const getProducers = async (req, res) => {
 
     const pool = await getConnection();
     const result = await pool.request().query('SELECT * FROM producers');
-
-    console.log(result);
-
-    res.json('getProducers');
+    res.json(result.recordset);
 
 };
