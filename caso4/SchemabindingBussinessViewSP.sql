02539b2811096cafb9435175dcc2499c35eb5d3e
TomasG10 <94805682+TomasG10@users.noreply.github.com>
2023-05-26 22:59:11 -0600

    schemabinding final adds mostly for demonstration

--- caso4/SchemabindingBussinessViewSP.sql
+++ caso4/SchemabindingBussinessViewSP.sql
@@ -1,4 +1,37 @@
+
+--Creacion del view
 CREATE VIEW dbo.RecyclingView
+
+AS
+SELECT
+    sc.service_contract_id,
+    sc.producer_id,
+    sc.start_date,
+    sc.end_date,
+    cl.collection_log_id,
+    cl.collection_point_id,
+    cl.datetime,
+    cl.responsible_person_id,
+    rc.recycling_contract_id,
+    i.invoice_id,
+    i.producer_id AS invoice_producer_id,
+    i.invoice_number,
+    i.invoice_date,
+    i.invoice_due_date,
+    t.transaction_id,
+    t.transaction_date,
+    p.payment_id,
+    p.payment_date,
+    p.producer_id AS payment_producer_id
+FROM dbo.service_contracts sc
+JOIN dbo.collection_log cl ON cl.service_contract_id = sc.service_contract_id
+JOIN dbo.recycling_contracts rc ON rc.service_contract_id = sc.service_contract_id
+JOIN dbo.invoices i ON i.producer_id = sc.producer_id
+JOIN dbo.transactions t ON t.currency_id = i.currency_id
+JOIN dbo.payments p ON p.producer_id = i.producer_id;
+
+--Se añade el schemabinding al view
+ALTER VIEW dbo.RecyclingView
 WITH SCHEMABINDING
 AS
 SELECT
@@ -27,3 +60,14 @@ JOIN dbo.recycling_contracts rc ON rc.service_contract_id = sc.service_contract_
 JOIN dbo.invoices i ON i.producer_id = sc.producer_id
 JOIN dbo.transactions t ON t.currency_id = i.currency_id
 JOIN dbo.payments p ON p.producer_id = i.producer_id;
+
+-----------------------------------
+--Alters para prueba de dependencia
+ALTER TABLE payments
+DROP COLUMN payment_date
+
+ALTER TABLE payments
+ADD payment_date DATETIME NOT NULL
+-----------------------------------
+
+DROP VIEW IF EXISTS dbo.RecyclingView
