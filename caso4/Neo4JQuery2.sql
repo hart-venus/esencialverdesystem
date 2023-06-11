90fb225b17ffbfc5f2973a87687a7f50984d79a5
Ariel Leyva <aleyva1509@gmail.com>
2023-06-11 12:28:06 -0600

    second neo4j query

--- caso4/Neo4JQuery2.sql
+++ caso4/Neo4JQuery2.sql
@@ -1,11 +1,12 @@
 SELECT
-    locations.name as location,
-    locations.location_id as location_id,
+    producers.name as producer,
+    producers.producer_id as producer_id,
     sum(weight) as total_kg_sent,
-    cp_to.collection_point_id as collection_point_id,
-    cp_to.name as collection_point
+    COALESCE(companies.company_id, 0) as company_id,
+    COALESCE(companies.name, 'Esencial Verde') as company
 FROM
     collection_log
+    LEFT JOIN companies ON companies.company_id = collection_log.company_id
     JOIN recipient_log ON recipient_log.collection_log_id = collection_log.collection_log_id
     JOIN service_contracts ON service_contracts.service_contract_id = collection_log.service_contract_id
     JOIN producers ON producers.producer_id = service_contracts.producer_id
@@ -16,4 +17,4 @@ WHERE
     recipient_log.movement_type_id = 1
     -- AND cp_to.is_dropoff = 1
 GROUP BY
-    locations.name, locations.location_id, cp_to.collection_point_id, cp_to.name
+    producers.name, producers.producer_id, companies.company_id, companies.name
